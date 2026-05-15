68a8e00a209f8059aa978ff9bbab964ed84f1a48
wbs0l <zerpad15@gmail.com>
2026-05-14 20:52:06 -0400

    cambio de conexion bbdd localhost a la real en SupaBase

--- Back/db/connection.js
+++ Back/db/connection.js
@@ -3,12 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sql = postgres({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+const sql = postgres(process.env.DATABASE_URL,{
+    ssl: 'require'
 });
 
 export default sql;
