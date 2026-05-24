cae7ec7d071400cab1ae273c3ed4bce09bff0288
wbs0l <zerpad15@gmail.com>
2026-05-24 00:18:21 -0400

    fix cors

--- Back/src/middlewares/cors.js
+++ Back/src/middlewares/cors.js
@@ -1,14 +1,14 @@
 import cors from 'cors';
 
 const ACCEPTED_ORIGINS= ['https://rinconplus.netlify.app',
-  'https://www.rinconplus.netlify.app', 'http://localhost:5173']; 
+  'https://www.rinconplus.netlify.app', 'https://rinconplus.onrender.com', 'http://localhost:5173']; 
 
 
 // al ser pasado como parametro acceptedOrigns se puede configurar la lista de origenes permitidos desde el index.js o cualquier otro archivo que importe este middleware, lo que hace que sea mas reutilizable y flexible para diferentes entornos o necesidades.
 export const corsMiddleware=({ acceptedOrigins= ACCEPTED_ORIGINS } = {})=>{ // antes de la flecha de function se agrega = {} en caso de que el parametro sea vacio y no se detenga 
     return cors({ // se configura el middleware de CORS para permitir solo los origenes especificados
      origin : (origin, callback) => {
-        console.log(origin);
+
         // permitir requests sin origin
             if (!origin) {
                 return callback(null, true);
